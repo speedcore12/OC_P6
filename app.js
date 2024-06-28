7b0ed87a6f5834d7a0a46dbf0f83256512ec5191
Manuel ZUANON <manuelzuanon@gmail.com>
2024-06-28 12:58:12 +0200

    ajout du 1er get et upload le data.json dans la db en ligne

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Book = require('./models/book'); 
 
 const app = express();
 
@@ -16,7 +17,16 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
+// Route pour récupérer tous les livres
+app.get('/api/books', (req, res, next) => {
+    Book.find()
+      .then(books => {
+        console.log(books); // Logue tous les livres dans la console
+        res.status(200).json(books); // Retourne les livres en réponse JSON
+      })
+      .catch(error => res.status(400).json({ error }));
+});    
 
 module.exports = app;
